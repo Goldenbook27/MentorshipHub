7c3089d7712913c19e2012357b0bcbff52c38681
Piyush <may.pranjali.naik@gmail.com>
2024-04-02 18:46:08 +0530

    schema

--- server/config/initDb..js
+++ server/config/initDb..js
@@ -5,12 +5,25 @@ const app = express()
 import goalRouter from '../../src/routes/routes.js'
 dotenv.config()
 
-const MONGO_URL=process.env.MONGO_URL
-mongoose.connect(MONGO_URL).then(()=>{
-    console.log("Connected to Database")
-}).catch((error)=>{
-    console.log(error.message)
-})
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGO_URL}`).then(()=>{
+            console.log("Connected to Database")
+        })
+        app.on("error",()=>{
+            console.log("ERROR: ",error)
+            throw error
+        })
+        app.listen(8000,(req,res)=>{
+            console.log("Server is listening to port 8000")
+        })
+
+    }  catch(error){
+        console.error("ERROR: ",error)
+        process.exit(1)
+    }
+    }  
 
 app.get('/',(req,res)=>{
     res.send("Hello")
@@ -18,6 +31,3 @@ app.get('/',(req,res)=>{
 
 app.use('/goal',goalRouter)
 
-app.listen(8000,(req,res)=>{
-    console.log("Server is listening to port 8000")
-})
